900ed732552c300dcb4833e896e22d168a5c9678
kmd2kmd <>
2019-07-30 23:47:44 +0900

    lint

--- bin/aws-cdk-eks.ts
+++ bin/aws-cdk-eks.ts
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import {App} from "@aws-cdk/core";
-import {NetworkStack} from "../lib/network-stack";
-import {EksStack} from "../lib/eks-stack";
+import { App } from "@aws-cdk/core";
+import { NetworkStack } from "../lib/network-stack";
+import { EksStack } from "../lib/eks-stack";
 
 const app = new App();
 
-const region = process.env.CDK_DEFAULT_REGION || 'ap-northeast-1';
+const region = process.env.CDK_DEFAULT_REGION || "ap-northeast-1";
 
-
-const networkStack = new NetworkStack(app, 'NetworkStack', {env:{region: region}});
-const eksStack = new EksStack(app, 'EksStack', {
-    env: {
-        region: region
-    },
-    vpc: networkStack.vpc
+const networkStack = new NetworkStack(app, "NetworkStack", {
+  env: { region: region }
+});
+const eksStack = new EksStack(app, "EksStack", {
+  env: {
+    region: region
+  },
+  vpc: networkStack.vpc
 });
--- lib/eks-stack.ts
+++ lib/eks-stack.ts
@@ -1,41 +1,42 @@
-import {Construct, Stack, StackProps} from "@aws-cdk/core";
-import {Cluster} from "@aws-cdk/aws-eks";
-import {InstanceClass, InstanceSize, InstanceType, IVpc} from "@aws-cdk/aws-ec2";
-import {ManagedPolicy} from "@aws-cdk/aws-iam";
-
+import { Construct, Stack, StackProps } from "@aws-cdk/core";
+import { Cluster } from "@aws-cdk/aws-eks";
+import {
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  IVpc
+} from "@aws-cdk/aws-ec2";
+import { ManagedPolicy } from "@aws-cdk/aws-iam";
 
 interface EksStackProps extends StackProps {
-    vpc: IVpc;
+  vpc: IVpc;
 }
 
 export class EksStack extends Stack {
-    public readonly cluster: Cluster;
+  public readonly cluster: Cluster;
 
-    constructor(scope: Construct, id: string, props: EksStackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props: EksStackProps) {
+    super(scope, id, props);
 
-        this.cluster = new Cluster(this, 'ControlPlane', {
-            vpc: props.vpc,
-        });
-        const asg = this.cluster.addCapacity('WorkerNodes', {
-            instanceType: InstanceType.of(InstanceClass.M5, InstanceSize.LARGE),
-            minCapacity: 2,
-            maxCapacity: 6,
-            desiredCapacity: 4,
-            spotPrice: "0.50"
-        });
+    this.cluster = new Cluster(this, "ControlPlane", {
+      vpc: props.vpc
+    });
+    const asg = this.cluster.addCapacity("WorkerNodes", {
+      instanceType: InstanceType.of(InstanceClass.M5, InstanceSize.LARGE),
+      minCapacity: 2,
+      maxCapacity: 6,
+      desiredCapacity: 4,
+      spotPrice: "0.50"
+    });
 
-        const managedPolicyNames = [
-            'service-role/AmazonEC2RoleforSSM',
-            'ElasticLoadBalancingFullAccess'
-        ];
-        for (let managedPolicyName of managedPolicyNames) {
-            asg.role.addManagedPolicy(
-                ManagedPolicy.fromAwsManagedPolicyName(
-                    managedPolicyName
-                )
-            )
-        }
-
+    const managedPolicyNames = [
+      "service-role/AmazonEC2RoleforSSM",
+      "ElasticLoadBalancingFullAccess"
+    ];
+    for (let managedPolicyName of managedPolicyNames) {
+      asg.role.addManagedPolicy(
+        ManagedPolicy.fromAwsManagedPolicyName(managedPolicyName)
+      );
     }
+  }
 }
